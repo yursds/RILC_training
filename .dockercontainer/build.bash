1155b284b432ef92c0fe7e9414af938f9f0a774a
yurs <yursds@outlook.com>
2024-11-19 08:43:07 +0000

    docker image

---
+++ .dockercontainer/build.bash
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+IMAGE_NAME=rilc_training
+
+# =============================== Preliminaries ============================== #
+
+mkdir -p ~/.vscode ~/.vscode-server ~/.config/Code
+
+# =============================== Help Function ============================== #
+
+helpFunction()
+{
+    echo ""
+    echo "Usage: $0 [-r]"
+    echo -e "\t-h   --help      Print the help."
+    echo -e "\t-r   --rebuild   Rebuild the image."
+    exit 1 # Exit script after printing help
+}
+
+# =============================== Build Options ============================== #
+
+# Initialie the build options
+REBUILD=0
+
+# Auxiliary functions
+die() { echo "$*" >&2; exit 2; }  # complain to STDERR and exit with error
+needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
+no_arg() { if [ -n "$OPTARG" ]; then die "No arg allowed for --$OPT option"; fi; }
+
+# Get the script options. This accepts both single dash (e.g. -a) and double dash options (e.g. --all)
+while getopts hr-: OPT; do
+    # support long options: https://stackoverflow.com/a/28466267/519360
+    if [ "$OPT" = "-" ]; then     # long option: reformulate OPT and OPTARG
+        OPT="${OPTARG%%=*}"       # extract long option name
+        OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
+        OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
+    fi
+    case "$OPT" in
+        h | help )      no_arg; helpFunction ;;
+        r | rebuild )   no_arg; REBUILD=1 ;;
+        ??* )           die "Illegal option --$OPT" ;;  # bad long option
+        ? )             exit 2 ;;  # bad short option (error reported via getopts)
+    esac
+done
+shift $((OPTIND-1)) # remove parsed options and args from $@ list
+
+# ========================= Pull And Build The Image ========================= #
+
+# lacal image not need pull
+# docker pull $BASE_IMAGE
+
+UID="$(id -u $USER)"
+GID="$(id -g $USER)"
+
+if [ "$REBUILD" -eq 1 ]; then
+    cache="--no-cache"
+else
+    cache=""
+fi
+
+docker build \
+    ${cache} \
+    --build-arg BASE_IMAGE=$BASE_IMAGE \
+    --build-arg UID=${UID} \
+    --build-arg GID=${GID} \
+    --build-arg USER=${USER} \
+    --build-arg "PWDR=$PWD" \
+    -t $IMAGE_NAME .
+
